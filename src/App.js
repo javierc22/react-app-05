44a57608006d275d196c0624862aea9b0073168a
javierc22 <javier.ec.22@gmail.com>
2020-06-25 22:16:06 -0400

    Deployment de la APP

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
     const consultarAPI = async () => {
       if (consultar) {
         const appId = 'ff650ad3329f28f437dd20693b2dfd68';
-        const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
   
         const respuesta = await fetch(url);
         const resultado = await respuesta.json();
